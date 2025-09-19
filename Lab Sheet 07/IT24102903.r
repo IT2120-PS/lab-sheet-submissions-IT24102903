40c95623408056e54fc87d7f6275ad0bcd5d7f19
IT24102903 <it24102903@my.sliit.lk>
2025-09-19 17:15:01 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102903.r
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\Asus\\Desktop\\IT24102903Lab07")
+#question 01
+min_time <- 0
+max_time <- 40
+lower <- 10
+upper <- 25
+prob <- punif(upper, min_time, max_time) - punif(lower, min_time,max_time)
+prob
+#question 02
+lambda <- 1/3
+t <- 2
+prob <- pexp(t, rate = lambda)
+prob
+#question 03 
+#part i
+mean_iq <- 100
+sd_iq <- 15
+prob_above_130 <- 1 - pnorm(130, mean = mean_iq,sd = sd_iq)
+prob_above_130
+#part ii
+iq_95th <-pnorm(0.95, mean = mean_iq , sd = sd_iq)
+iq_95th
